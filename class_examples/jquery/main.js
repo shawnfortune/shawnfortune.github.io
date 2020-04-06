75bf75f75a04709c3e3d08fbe181b044885dcbf6
shawnfortune <shawnfortune@miami.edu>
2020-04-06 13:13:33 -0400

    jqueryFIXED

--- class_examples/jquery/main.js
+++ class_examples/jquery/main.js
@@ -1,6 +1,39 @@
-    $(document).ready(function() {
-      //all js goes in here
-      console.log("we loaded")
+$(document)
+  .ready(function() {
+    //all js goes in here
+    console.log("we loaded");
 
-      $("title").css("color", "#bada55");
-    });
+    $("#button1")
+      .click(function() {
+        // Code that runs when you click on the part of the page that has id="button1".
+        $("#title")
+          .addClass("big-text");
+      });
+
+    $("#button2")
+      .click(function() {
+        // stuff we put here will happen when you click button2.
+        // in this case we remove the class called big-text from
+        // the part of the page that has id=title
+        $("#title")
+          .removeClass("big-text");
+      });
+
+    $("p")
+      .mouseover(function() {
+        // when your mouse is over a paragraph, the font size will become 21px
+        $("p")
+          .css("font-size", "21px");
+      });
+
+    $("p")
+      .mouseleave(function() {
+        // when your mouse leaves a paragraph the font-size will get smaller
+        // and the background-color of the paragraph will become yellow
+        $("p")
+          .css("background-color", "yellow");
+        $("p")
+          .css("font-size", "16px");
+      });
+
+  });
